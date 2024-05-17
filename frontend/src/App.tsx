7224cb0b2556303adc2d9948c45533436e3e24c8
seungki <sysh9498@gmail.com>
2024-05-17 11:21:57 +0900

    [FE] feat: 글로벌 스타일변경

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import OnBoard from "./pages/OnBoard";
+import Main from "./pages/Main";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" Component={OnBoard} />
+        <Route path="/main" Component={Main} />
       </Routes>
     </BrowserRouter>
   );
