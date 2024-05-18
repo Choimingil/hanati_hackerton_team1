1e0fe7c7ab701ebec32d35c86c082514d57a0878
hgene2452 <hgene0929@gmail.com>
2024-05-18 17:40:10 +0900

    [Feat] subscribe save api

--- backend/src/main/java/com/hanati/team1/src/players/service/ProspectService.java
+++ backend/src/main/java/com/hanati/team1/src/players/service/ProspectService.java
@@ -1,10 +1,14 @@
 package com.hanati.team1.src.players.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
+import com.hanati.team1.src.customers.entities.Subscribe;
+import com.hanati.team1.src.customers.repository.SubscribeRepository;
 import com.hanati.team1.src.players.entities.Player;
 import com.hanati.team1.src.players.models.GetProspectDetailRes;
 import com.hanati.team1.src.players.models.GetProspectRes;
@@ -14,10 +18,12 @@ import com.hanati.team1.src.players.repository.PlayerRepository;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @Service
 public class ProspectService {
 
 	private final PlayerRepository playerRepository;
+	private final SubscribeRepository subscribeRepository;
 
 	public List<GetProspectRes> getProspectList(String keyword) {
 		if (!StringUtils.hasText(keyword)) {
@@ -32,9 +38,28 @@ public class ProspectService {
 			.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유망주입니다."));
 	}
 
+	@Transactional
 	public void saveProspect(PostProspectReq postProspectReq) {
 		// TODO: profile, video 쪽 S3에 미리 넣어둔 파일저장경로 url 고정으로 박아두기
 		Player player = postProspectReq.toEntity("temp", "temp");
 		playerRepository.save(player);
+	}
+
+	@Transactional
+	public void saveSubscribe(long prospectId) {
+		long userId = 1L;
+
+		Optional<Subscribe> optionalSubscribe = subscribeRepository.findByPlayerIdAndUserId(prospectId, userId);
+		if (optionalSubscribe.isPresent()) {
+			Subscribe subscribe = optionalSubscribe.get();
+			subscribe.saveSubscribe();
+			return;
+		}
+
+		Subscribe subscribe = Subscribe.builder()
+			.userId(userId)
+			.playerId(prospectId)
+			.build();
+		subscribeRepository.save(subscribe);
 	}
 }
