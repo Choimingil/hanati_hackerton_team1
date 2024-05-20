b9c6f6fcef119f29d29f4983885efb05acfd3ba2
hgene2452 <hgene0929@gmail.com>
2024-05-20 22:34:33 +0900

    [Fix] add import

--- backend/src/main/java/com/hanati/team1/parsers/PositionParser.java
+++ backend/src/main/java/com/hanati/team1/parsers/PositionParser.java
@@ -1,6 +1,8 @@
 package com.hanati.team1.parsers;
 
 import com.hanati.team1.src.players.entities.Position;
+import com.hanati.team1.src.players.repository.PositionRepository;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
--- backend/src/main/java/com/hanati/team1/src/players/repository/PositionRepository.java
+++ backend/src/main/java/com/hanati/team1/src/players/repository/PositionRepository.java
@@ -1,4 +1,8 @@
 package com.hanati.team1.src.players.repository;
 
-public interface PositionRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.hanati.team1.src.players.entities.Position;
+
+public interface PositionRepository extends JpaRepository<Position, Long> {
 }
