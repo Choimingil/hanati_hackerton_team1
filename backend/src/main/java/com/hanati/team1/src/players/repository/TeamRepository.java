1e2173d2361b320f5cb5fcf02bc7c51f56d73b32
hgene2452 <hgene0929@gmail.com>
2024-05-20 22:16:22 +0900

    [Feat] add query

--- backend/src/main/java/com/hanati/team1/src/players/repository/TeamRepository.java
+++ backend/src/main/java/com/hanati/team1/src/players/repository/TeamRepository.java
@@ -18,7 +18,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
 	@Query(
 		"select new com.hanati.team1.src.players.models.GetPlayerListRes(p.playerId, tb.playerBacknum, p.playerName, pp.positionName, tt.tokenPrice, 'profile') "
 			+ "from Player p "
-			+ "inner join TeamByPlayer tb on tb.playerId = p.playerId and tb.endDate >= now()"
+			+ "inner join TeamByPlayer tb on tb.playerId = p.playerId and tb.endDate >= now() "
 			+ "inner join Team t on t.teamId = tb.teamId "
 			+ "inner join Position pp on pp.playerId = p.playerId "
 			+ "inner join Trade tt on tt.playerId = p.playerId "
