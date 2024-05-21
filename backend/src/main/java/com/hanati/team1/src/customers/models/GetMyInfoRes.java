16a4925d702c4ad6ca4d48f5aab91bdf473d9376
hgene2452 <hgene0929@gmail.com>
2024-05-21 15:58:01 +0900

    [Chore] chore query

--- backend/src/main/java/com/hanati/team1/src/customers/models/GetMyInfoRes.java
+++ backend/src/main/java/com/hanati/team1/src/customers/models/GetMyInfoRes.java
@@ -3,7 +3,7 @@ package com.hanati.team1.src.customers.models;
 public record GetMyInfoRes(
 	String name,
 	String profile,
-	int totalAsset,
-	int totalCntToken
+	long totalAsset,
+	long totalCntToken
 ) {
 }
