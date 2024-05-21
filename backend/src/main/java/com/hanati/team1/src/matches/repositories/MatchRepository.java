633beec17799d210f28d53c6f424d494be64256c
hgene2452 <hgene0929@gmail.com>
2024-05-21 14:37:58 +0900

    [Feat] add query

--- backend/src/main/java/com/hanati/team1/src/matches/repositories/MatchRepository.java
+++ backend/src/main/java/com/hanati/team1/src/matches/repositories/MatchRepository.java
@@ -11,10 +11,9 @@ import com.hanati.team1.src.matches.models.GetMatchListRes;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match,Long> {
-	@Query(
-		"select new com.hanati.team1.src.matches.models.GetMatchListRes(m.matchId, m.matchDate, m.playerStatus, m.playerPlaytime, m.playerScore, m.playerYcardNum) "
-			+ "from Match m "
-			+ "where m.playerId = :playerId "
-			+ "order by m.matchDate desc")
+	@Query("select new com.hanati.team1.src.matches.models.GetMatchListRes(m.matchId, m.matchDate, m.playerStatus, m.playerPlaytime, m.playerScore, m.playerYcardNum) "
+		+ "from Match m "
+		+ "where m.playerId = :playerId "
+		+ "order by m.matchDate desc")
 	List<GetMatchListRes> findAllMatches(long playerId);
 }
