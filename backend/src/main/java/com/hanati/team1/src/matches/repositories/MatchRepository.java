bbbef82fe76f9a22eb20a13834ac489c17168dba
hgene2452 <hgene0929@gmail.com>
2024-05-21 14:31:21 +0900

    [Feat] player detail matches api

--- backend/src/main/java/com/hanati/team1/src/matches/repositories/MatchRepository.java
+++ backend/src/main/java/com/hanati/team1/src/matches/repositories/MatchRepository.java
@@ -1,9 +1,16 @@
 package com.hanati.team1.src.matches.repositories;
 
-import com.hanati.team1.src.matches.entities.Match;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import com.hanati.team1.src.matches.entities.Match;
+import com.hanati.team1.src.matches.models.GetMatchListRes;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match,Long> {
+	@Query("")
+	List<GetMatchListRes> findAllMatches(long playerId);
 }
