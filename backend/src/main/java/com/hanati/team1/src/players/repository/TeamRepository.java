b98f8887b37722e661d6f8c78ff0e66c1ca5bf47
hgene2452 <hgene0929@gmail.com>
2024-05-22 11:19:58 +0900

    [Chore] chore query

--- backend/src/main/java/com/hanati/team1/src/players/repository/TeamRepository.java
+++ backend/src/main/java/com/hanati/team1/src/players/repository/TeamRepository.java
@@ -17,7 +17,7 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
 	List<GetTeamListRes> findAllTeams(@Param("keyword") String keyword);
 
 	@Query(
-		"select new com.hanati.team1.src.players.models.GetPlayerListRes(p.playerId, tb.playerBacknum, p.playerName, (select pp.positionName from Position pp where pp.playerId = p.playerId and pp.positionLevel = 10), (select abs(tt.tokenPrice) from Trade tt where tt.playerId = p.playerId order by tt.tradeDate desc limit 1), p.playerProfile) "
+		"select new com.hanati.team1.src.players.models.GetPlayerListRes(p.playerId, tb.playerBacknum, p.playerName, (select pp.positionName from Position pp where pp.playerId = p.playerId and pp.positionLevel = 10), (select tt.tokenPrice from Trade tt where tt.playerId = p.playerId order by tt.tradeDate desc limit 1), p.playerProfile) "
 			+ "from Player p "
 			+ "inner join TeamByPlayer tb on tb.playerId = p.playerId and tb.endDate >= now() "
 			+ "inner join Team t on t.teamId = tb.teamId "
