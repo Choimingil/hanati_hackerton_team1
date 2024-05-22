ddf5e16c948d10e1b8f947f7f07e8e1e2d96bf5f
hgene2452 <hgene0929@gmail.com>
2024-05-22 10:44:41 +0900

    [Chore] chore query

--- backend/src/main/java/com/hanati/team1/src/players/repository/TeamRepository.java
+++ backend/src/main/java/com/hanati/team1/src/players/repository/TeamRepository.java
@@ -17,11 +17,10 @@ public interface TeamRepository extends JpaRepository<Team, Long> {
 	List<GetTeamListRes> findAllTeams(@Param("keyword") String keyword);
 
 	@Query(
-		"select new com.hanati.team1.src.players.models.GetPlayerListRes(p.playerId, tb.playerBacknum, p.playerName, pp.positionName, (select tt.tokenPrice from Trade tt where tt.playerId = p.playerId order by tt.tradeDate desc limit 1), p.playerProfile) "
+		"select new com.hanati.team1.src.players.models.GetPlayerListRes(p.playerId, tb.playerBacknum, p.playerName, (select pp.positionName from Position pp where pp.playerId = p.playerId and pp.positionLevel = 10), (select tt.tokenPrice from Trade tt where tt.playerId = p.playerId order by tt.tradeDate desc limit 1), p.playerProfile) "
 			+ "from Player p "
 			+ "inner join TeamByPlayer tb on tb.playerId = p.playerId and tb.endDate >= now() "
 			+ "inner join Team t on t.teamId = tb.teamId "
-			+ "inner join Position pp on pp.playerId = p.playerId "
 			+ "where t.teamId = :teamId")
 	List<GetPlayerListRes> findAllPlayers(@Param("teamId") long teamId);
 }
