6ef7a2f9ba0f775b24dbc68813cef6df81d64c4a
sinhyeonchang <hcshin9999@gmail.com>
2024-05-22 11:15:27 +0900

    [feat] prospectlist api connect

--- frontend/src/pages/ProspectList.tsx
+++ frontend/src/pages/ProspectList.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Header from "../components/Header";
 import { ListContainer } from "./ProList";
 import ProspectUserBox from "../components/ProspectUserBox";
@@ -68,14 +69,41 @@ const data = [
 
 function ProspectList() {
   const navigation = useNavigate();
+
+  const [users, setUsers] = useState([{
+    id: 1,
+    name: "",
+    cntSub: 0,
+    profile: "",
+    position: "",
+  }]);
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/prospects')
+      .then(response => {
+        console.log(response);
+        setUsers(response.data.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+
   return (
     <>
       <Header title="유망주 목록" />
       <img src={CitizenTitle} alt="title_img" />
 
       <ProspectListContainer>
-        {data.map(dt => (
-          <ProspectUserBox id={dt.id} name={dt.name} cntSub={dt.cntSub} profile={dt.profile} position={dt.position} onClick={()=>navigation(`/prospectDetail/${dt.id}`)}/>
+        {users.map(dt => (
+          <ProspectUserBox
+            id={dt.id}
+            name={dt.name}
+            cntSub={dt.cntSub}
+            profile={dt.profile}
+            position={dt.position}
+            onClick={() => navigation(`/prospectDetail/${dt.id}`)} />
         ))}
       </ProspectListContainer>
       <UnderBar />
